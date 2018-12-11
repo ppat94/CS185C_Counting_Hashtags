8a70011cf5011e5de70a3002114979115e42ace9
GayatriHungund81194 <43119518+GayatriHungund81194@users.noreply.github.com>
2018-12-10 17:35:34 -0800

    Update script.sh

--- src/jdbc-hive/script.sh
+++ src/jdbc-hive/script.sh
@@ -2,7 +2,7 @@ hadoop fs -put -f /home/mapr/Desktop/CS185C_Counting_Hashtags/src/streamer/proce
 mvn clean install
 java -jar target/jdbc-hive-1.0-SNAPSHOT-jar-with-dependencies.jar jdbc:hive2://192.168.56.101:10000/default;ssl=false
 yes | cp -rf record.json /home/mapr/Desktop/CS185C_Counting_Hashtags/pure-js/record.json
-cd /home/mapr/Desktop/CS185C_Counting_Hashtags/src/pure-js/
+cd /home/mapr/Desktop/CS185C_Counting_Hashtags/pure-js/
 npm install
 npm start
  
