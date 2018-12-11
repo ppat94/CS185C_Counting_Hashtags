942020116c458bb7b6749c8185615f5d97e5b3ad
AparnaKale <43126210+AparnaKale@users.noreply.github.com>
2018-12-10 16:08:56 -0800

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-#yum install zip -y
-#yum install unzip -y
-
-#cd /home/mapr/Desktop/CS185C_Counting_Hashtags/data/
-#unzip hash.txt.zip 
-#mv -f hash.txt /home/mapr/Desktop/CS185C_Counting_Hashtags/src/streamer/processed/hash.txt
  
 counter=0
-#yum install python-pip -y
-#pip install tweepy
-#pip install numpy
 cd /home/mapr/Desktop/CS185C_Counting_Hashtags/src/streamer/
 while [ $counter -lt 3 ]
 do
    echo "Creating Tweeter Live Stream File"
-   python3.6 TweeterLiveStreamer.py
+   python3.6 TwitterLiveStreamer.py
    counter=`expr $counter + 1`
 done
 
